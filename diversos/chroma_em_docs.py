f07ecb9b7a581bf916686dfd56d8a884a06830a6
André Ramos <andre-rammos@hotmail.com>
2020-05-21 10:28:30 -0300

    add salvamento automatico em pasta dos frames com docs cortados.

--- diversos/chroma_em_docs.py
+++ diversos/chroma_em_docs.py
@@ -4,7 +4,7 @@
 import cv2 as cv
 import numpy as np
 
-video_cam = cv.VideoCapture('videos/chroma1.mp4')
+video_chroma = cv.VideoCapture('videos/chroma1.mp4')
 
 
 def nothing(x):
@@ -18,9 +18,11 @@ cv.createTrackbar("min_v", "Regulagens", 0, 255, nothing)
 cv.createTrackbar("max_h", "Regulagens", 0, 180, nothing)
 cv.createTrackbar("max_s", "Regulagens", 0, 255, nothing)
 cv.createTrackbar("max_v", "Regulagens", 0, 255, nothing)
+count = 0
+ret, frame1 = video_chroma.read()
+img_cortada = np.ndarray(shape=frame1.shape, dtype=int, order='F')
 
-
-while video_cam.isOpened():
+while video_chroma.isOpened():
 
     min_h = cv.getTrackbarPos("min_h", "Regulagens")
     min_s = cv.getTrackbarPos("min_s", "Regulagens")
@@ -30,7 +32,7 @@ while video_cam.isOpened():
     max_s = cv.getTrackbarPos("max_s", "Regulagens")
     max_v = cv.getTrackbarPos("max_v", "Regulagens")
 
-    sucesso, frame_cam = video_cam.read()
+    sucesso, frame_cam = video_chroma.read()
     frame_cam = cv.resize(frame_cam, (300, 400))
     frame_cam_hsv = cv.cvtColor(frame_cam, cv.COLOR_BGR2HSV)
     frame_cam_blur = cv.blur(frame_cam_hsv, (5, 5), 5)
@@ -46,8 +48,12 @@ while video_cam.isOpened():
         x, y, w, h = cv.boundingRect(objeto)
         if 5000 < cv.contourArea(objeto) < 15000:
             print(len(objetos))
+            img_cortada = frame_cam[y:y + h, x:x + w]
             cv.rectangle(frame_cam, (x, y), (x+w, h+y), (255, 0, 0, 0.2), 2)
 
+    image = cv.resize(img_cortada, (img_cortada.shape[1], img_cortada.shape[0]))
+    cv.imwrite("frames/frame%d.jpg" % count, image)
+    count += 1
     cv.imshow("mask_inv", mask_inv)
     cv.imshow("imagem_com_mascara", imagem_com_mascara)
     cv.imshow("frame_cam", frame_cam)
@@ -56,4 +62,4 @@ while video_cam.isOpened():
         break
 
 cv.destroyAllWindows()
-video_cam.release()
+video_chroma.release()
